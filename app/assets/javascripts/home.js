0c504d47cf29a69d9f21a910b1ce19b7add9ad65
Carl Timm <ctiv@hotmail.com>
2015-03-02 16:26:55 -0800

    Initial commit

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+
+	$("#devSend").on("click", function(){
+	   alert("As a starting point, this should pull up an interstitial that allows the user to send the link to someone on their dev/tech team. This should be trackable and used as one Continued Action data point to help evaluate how this homepage is moving potential customers down the funnel.  Additionally, if the recepient is tracked, future leads/sales can be linked back to this continued action.");
+	});		
+});
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  
+  def index  
+    respond_to do |format|               
+      format.js
+      format.html 
+    end
+  end
+end
